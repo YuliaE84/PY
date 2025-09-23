39cefac8a7ff8d4731fe634528633fa942089b93
YuliaE84 <yulchik_k@list.ru>
2025-09-23 12:06:25 +0500

    Внесла изменение во 2 задание

--- 06K_lesson/test_02_calc.py
+++ 06K_lesson/test_02_calc.py
@@ -5,40 +5,46 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def test_calculator():
-    driver = webdriver.Chrome()  
-    driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+@pytest.fixture(scope="module")
+def browser():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
+
+@pytest.mark.usefixtures("browser")
+def test_calculator(browser): 
+    browser.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
 
     try:
-        delay_input = WebDriverWait(driver, 10).until(
+        delay_input = WebDriverWait(browser, 10).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, '#delay'))
         )
         delay_input.clear()
         delay_input.send_keys('45')
 
-        seven_button = WebDriverWait(driver, 10).until(
+        seven_button = WebDriverWait(browser, 10).until(
             EC.element_to_be_clickable((By.XPATH, "//span[text()='7']"))
         )
         seven_button.click()
 
-        plus_button = WebDriverWait(driver, 10).until(
+        plus_button = WebDriverWait(browser, 10).until(
             EC.element_to_be_clickable((By.XPATH, "//span[text()='+']"))
         )
         plus_button.click()
 
-        eight_button = WebDriverWait(driver, 10).until(
+        eight_button = WebDriverWait(browser, 10).until(
             EC.element_to_be_clickable((By.XPATH, "//span[text()='8']"))
         )
         eight_button.click()
 
-        equals_button = WebDriverWait(driver, 10).until(
+        equals_button = WebDriverWait(browser, 10).until(
             EC.element_to_be_clickable((By.XPATH, "//span[text()='=']"))
         )
         equals_button.click()
 
-        result_field = WebDriverWait(driver, 50).until(
+        result_field = WebDriverWait(browser, 50).until(
             EC.text_to_be_present_in_element((By.ID, 'result'), '15')
         )
     
     finally:
-        driver.quit()
+         pass
